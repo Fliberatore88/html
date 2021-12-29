ba23e0db364bbbe3c688687a9335c742f7ffb64b
Fliberatore88 <federico.liberatore88@gmail.com>
2021-12-29 09:39:32 -0300

    Agregue el path.join para que funcione el index.html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 
 app.get ('/', (req,res) => {
-    res.sendFile (__dirname, '/index.html')
+    res.sendFile (path.join(__dirname, '/index.html'))
 })
 app.listen(3000, () => {
     console.log ('El Servidor levantó correctamente')
